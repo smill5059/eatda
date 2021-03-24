5d84ce41108cf0b1441af7acf1ca9ed20dc65480
chloe <danonieee@naver.com>
2021-03-24 17:58:48 +0900

    Modify: 캘린더 컴포넌트에 리덕스 적용 * src/store/modules/baseDate.js 수정 * src/components/calendar/calHeader.js 수정 * src/components/calendar/calBody.js 수정

--- eatda_fe/src/App.js
+++ eatda_fe/src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Route, BrowserRouter, Switch, } from "react-router-dom";
-
 import Layout from './pages/layout/layout';
 import Main from './pages/main/main';
 // 약속 생성
@@ -13,7 +12,6 @@ function App() {
     <BrowserRouter>
       <div className="Layout">
         <div className="Nav">
-        
         </div>
         <div className="Content">
           <Switch>
