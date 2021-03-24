9ab3c7b96b06e55042b8eb15e041dd9800f2bc6b
구본혁 <qhsgur0126@gmail.com>
2021-03-24 22:37:42 +0900

    Modify : 만난 이후 약속 보기 - src/assets/scss/meetingReview.scss : 약속(후) 보기 - src/components/meeting/meetInfo.js : 약속(전) 보기 화면 구현 완료 - src/components/meeting/meetReview.js : 약속(후) 보기 화면 구현 완료 - src/index.scss : 새로 만든 SCSS 파일 가져오기 - src/pages/meeing/read.js : 약속 보기 컴포넌트 가져오기

--- eatda_fe/src/components/meeting/meetInfo.js
+++ eatda_fe/src/components/meeting/meetInfo.js
@@ -1,12 +1,66 @@
 import React from "react";
-import './App.css';
+import { Button, Row, Col } from "antd";
 
-function App() {
+function MeetingInfo() {
   return (
-    <div className="App">
-
-    </div>
+    <div className="contentBody meetingReadContent">
+        <Row justify="end" className="meetingReadTitle">
+          <p>수림이와 홍대 나들이</p>
+        </Row>
+        <Col className="meetingReadMap"></Col>
+        <Col className="meetingReadStore">
+            <Row className="meetingReadStoreItem">
+                <Col span={3}></Col>
+                <Col span={21} className="meetingReadStoreName">가게 이름</Col>
+            </Row>
+            <Row className="meetingReadStoreItem">
+                <Col span={3}></Col>
+                <Col span={21} className="meetingReadStoreName">가게 이름가게 이름가게 이름가게 이름가게 이름가게 이름가게 이름가게 이름가게 이름가게 이름가게 이름</Col>
+            </Row>
+            <Row className="meetingReadStoreItem">
+                <Col span={3}></Col>
+                <Col span={21} className="meetingReadStoreName">가게 이름</Col>
+            </Row>
+            <Row className="meetingReadStoreItem">
+                <Col span={3}></Col>
+                <Col span={21} className="meetingReadStoreName">가게 이름</Col>
+            </Row>
+        </Col>
+        <Col className="meetingReadFriend">
+          <p>만나는 사람</p>
+          <div className="meetingReadFriendList">
+            <Row className="meetingReadFriendItem">
+              <Col span={8} className="meetingReadFriendImage">
+                <img src="https://via.placeholder.com/30"/>
+              </Col>
+              <Col span={16} className="meetingReadFriendName">
+                왕왕왕asdasdasdasdasdasdasd
+              </Col>
+            </Row>
+            <Row className="meetingReadFriendItem">
+              <Col className="meetingReadFriendImage">
+                <img src="https://via.placeholder.com/30"/>
+              </Col>
+              <Col className="meetingReadFriendName">
+                왕왕왕
+              </Col>
+            </Row>
+            <Row className="meetingReadFriendItem">
+              <Col className="meetingReadFriendImage">
+                <img src="https://via.placeholder.com/30"/>
+              </Col>
+              <Col className="meetingReadFriendName">
+                왕왕왕
+              </Col>
+            </Row>
+            
+          </div>
+        </Col>
+        <Row className="meetingReadDone" justify="end">
+          <Button className="meetingReadDoneButton">만났어요</Button>
+        </Row>
+      </div>
   );
 }
 
-export default App;
+export default MeetingInfo;
--- eatda_fe/src/pages/meeting/read.js
+++ eatda_fe/src/pages/meeting/read.js
@@ -1,5 +1,7 @@
 import React, { useEffect } from "react";
 import { Button, Dropdown, Menu, Row, Col, Image } from "antd";
+import MeetingInfo from "../../components/meeting/meetInfo";
+import MeetingReview from "../../components/meeting/meetReview"
 
 function MeetingRead(props) {
   const { meetingId } = props.match.params;
@@ -16,6 +18,8 @@ function MeetingRead(props) {
       </Menu.Item>
     </Menu>
   );
+
+
 
   useEffect(() => {
     // 지도 로딩
@@ -37,46 +41,11 @@ function MeetingRead(props) {
           </Dropdown>
         </Col>
       </Row>
-      <div className="contentBody meetingReadContent">
-        <Row justify="end" className="meetingReadTitle">
-          <p>수림이와 홍대 나들이</p>
-        </Row>
-        <Col className="meetingReadMap"></Col>
-        <Col className="meetingReadStore"></Col>
-        <Col className="meetingReadFriend">
-          <p>만나는 사람</p>
-          <div className="meetingReadFriendList">
-            <Row className="meetingReadFriendItem">
-              <Col span={8} className="meetingReadFriendImage">
-                <img src="https://via.placeholder.com/30"/>
-              </Col>
-              <Col span={16} className="meetingReadFriendName">
-                왕왕왕asdasdasdasdasdasdasd
-              </Col>
-            </Row>
-            <Row className="meetingReadFriendItem">
-              <Col className="meetingReadFriendImage">
-                <img src="https://via.placeholder.com/30"/>
-              </Col>
-              <Col className="meetingReadFriendName">
-                왕왕왕
-              </Col>
-            </Row>
-            <Row className="meetingReadFriendItem">
-              <Col className="meetingReadFriendImage">
-                <img src="https://via.placeholder.com/30"/>
-              </Col>
-              <Col className="meetingReadFriendName">
-                왕왕왕
-              </Col>
-            </Row>
-            
-          </div>
-        </Col>
-        <Row className="meetingReadDone" justify="end">
-          <Button className="meetingReadDoneButton">만났어요</Button>
-        </Row>
-      </div>
+      {/* 조건문으로 분할 */}
+      {/* <MeetingInfo></MeetingInfo> */}
+      <MeetingReview></MeetingReview>
+      
+      
     </div>
   );
 }
