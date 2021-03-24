715b3ba9620d423ab19c3ec9f58f446a5c8a0185
jiuen <wldms9604@naver.com>
2021-03-24 14:46:10 +0900

    Modify : 캘린더, 타임라인 추가 및 VO 변경  - eatda_be/src/main/java/com/ssafy/eatda/controller/UserController.java : 파라미터 형식 변경  - eatda_be/src/main/java/com/ssafy/eatda/controller/MeetingController.java : seq 변경  - eatda_be/src/main/java/com/ssafy/eatda/controller/MainController.java : 캘린더, 타임라인(기본, 날짜, 친구, 제목) 추가  - eatda_be/src/main/java/com/ssafy/eatda/service/MainService.java : MainService 인터페이스  - eatda_be/src/main/java/com/ssafy/eatda/service/MainServiceImpl.java : 캘린더, 타임라인(기본, 날짜, 친구, 제목) 추가  - eatda_be/src/main/resources/static/index.html : 로그인 테스트 수정  - eatda_be/src/main/resources/static/index3.html : FE <-> BE ObjectId 테스트 수정  - eatda_be/src/main/resources/static/index4.html : FE <-> BE Date 테스트 수정  - eatda_be/src/main/java/com/ssafy/eatda/vo/Comment.java : seq 수정  - eatda_be/src/main/java/com/ssafy/eatda/vo/Schedule.java : seq, id 수정  - eatda_be/src/main/java/com/ssafy/eatda/vo/ScheduleResult.java : 프론트 전송용 vo 추가  - eatda_be/src/main/java/com/ssafy/eatda/vo/Score.java : seq 수정  - eatda_be/src/main/java/com/ssafy/eatda/repository/ScheduleRepository.java : Schedule Repository 추가

--- eatda_be/src/main/java/com/ssafy/eatda/service/MainServiceImpl.java
+++ eatda_be/src/main/java/com/ssafy/eatda/service/MainServiceImpl.java
@@ -1,12 +1,19 @@
 package com.ssafy.eatda.service;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.ssafy.eatda.repository.ProfileRepository;
+import com.ssafy.eatda.repository.ScheduleRepository;
 import com.ssafy.eatda.repository.UserRepository;
+import com.ssafy.eatda.vo.Profile;
 import com.ssafy.eatda.vo.Schedule;
+import com.ssafy.eatda.vo.ScheduleResult;
+import com.ssafy.eatda.vo.User;
 
 @Service
 public class MainServiceImpl implements MainService {
@@ -14,27 +21,180 @@ public class MainServiceImpl implements MainService {
   @Autowired
   private UserRepository userRepository;
 
-  @Override
-  public List<Schedule> getSchedules(int userSeq, Date requestDate) {
+  @Autowired
+  private ScheduleRepository scheduleRepository;
 
-    // User user = userRepository.findBySeq(userSeq);
-    // if (user == null)
-    // return null;
-    //
-    // Calendar cal = Calendar.getInstance();
-    // cal.setTime(requestDate);
-    // int year = cal.get(Calendar.YEAR);
-    // int month = cal.get(Calendar.MONTH);
-    //
-    ArrayList<Schedule> result = new ArrayList<Schedule>();
-    //
-    // for (Schedule s : user.getSchedules()) {
-    // cal.setTime(s.getMeetDate());
-    // if (cal.get(Calendar.YEAR) == year && cal.get(Calendar.MONTH) == month) {
-    // result.add(s);
-    // }
-    // }
-    //
+  @Autowired
+  private ProfileRepository profileRepository;
+
+  // 캘린더
+  @Override
+  public List<ScheduleResult> getSchedules(int userSeq, Date requestDate) {
+
+    User user = userRepository.findBySeq(userSeq);
+    if (user == null)
+      return null;
+
+    Calendar cal = Calendar.getInstance();
+    cal.setTime(requestDate);
+    int year = cal.get(Calendar.YEAR);
+    int month = cal.get(Calendar.MONTH);
+
+    ArrayList<ScheduleResult> result = new ArrayList<ScheduleResult>();
+
+    for (ObjectId id : user.getSchedules()) {
+      Schedule s = scheduleRepository.findById(id).get();
+      cal.setTime(s.getMeetDate());
+      if (cal.get(Calendar.YEAR) == year && cal.get(Calendar.MONTH) == month) {
+        ScheduleResult sr = new ScheduleResult();
+        sr.copy(s);
+        ArrayList<Profile> profiles = new ArrayList<Profile>();
+        for (ObjectId userId : s.getParticipants()) {
+          Profile profile = profileRepository.findById(userId).get();
+          if (profile == null)
+            return null;
+          profiles.add(profile);
+        }
+        result.add(sr);
+      }
+    }
+
+    return result;
+  }
+
+  // 타임라인
+  @Override
+  public List<ScheduleResult> getTimeline(int userSeq) {
+    User user = userRepository.findBySeq(userSeq);
+    if (user == null)
+      return null;
+
+    ArrayList<ScheduleResult> result = new ArrayList<ScheduleResult>();
+
+    for (ObjectId id : user.getSchedules()) {
+      Schedule s = scheduleRepository.findById(id).get();
+      if (s.isCompleted()) {
+        ScheduleResult sr = new ScheduleResult();
+        sr.copy(s);
+        ArrayList<Profile> profiles = new ArrayList<Profile>();
+        for (ObjectId userId : s.getParticipants()) {
+          Profile profile = profileRepository.findById(userId).get();
+          if (profile == null)
+            return null;
+          profiles.add(profile);
+        }
+        result.add(sr);
+      }
+    }
+
+    return result;
+  }
+
+  // 타임라인 - 날짜필터
+  @Override
+  public List<ScheduleResult> getTimelineByDate(int userSeq, Date startDate, Date endDate) {
+    User user = userRepository.findBySeq(userSeq);
+    if (user == null)
+      return null;
+
+    ArrayList<ScheduleResult> result = new ArrayList<ScheduleResult>();
+
+    Calendar start = Calendar.getInstance();
+    Calendar end = Calendar.getInstance();
+
+    start.setTime(startDate);
+    start.add(Calendar.DATE, -1);
+    start.set(Calendar.HOUR_OF_DAY, 23);
+    start.set(Calendar.MINUTE, 59);
+    start.set(Calendar.SECOND, 59);
+
+    end.setTime(endDate);
+    end.add(Calendar.DATE, 1);
+    end.set(Calendar.HOUR_OF_DAY, 0);
+    end.set(Calendar.MINUTE, 0);
+    end.set(Calendar.SECOND, 0);
+
+    for (ObjectId id : user.getSchedules()) {
+      Schedule s = scheduleRepository.findById(id).get();
+      Calendar chk = Calendar.getInstance();
+      chk.setTime(s.getMeetDate());
+      if (start.before(chk) && end.after(chk) && s.isCompleted()) {
+        ScheduleResult sr = new ScheduleResult();
+        sr.copy(s);
+        ArrayList<Profile> profiles = new ArrayList<Profile>();
+        for (ObjectId userId : s.getParticipants()) {
+          Profile profile = profileRepository.findById(userId).get();
+          if (profile == null)
+            return null;
+          profiles.add(profile);
+        }
+        result.add(sr);
+      }
+    }
+
+    return result;
+  }
+
+  // 타임라인 친구필터
+  @Override
+  public List<ScheduleResult> getTimelineByFriend(int userSeq, int code) {
+    User user = userRepository.findBySeq(userSeq);
+    if (user == null)
+      return null;
+
+    User friend = userRepository.findBySeq(code);
+    if (friend == null)
+      return null;
+
+    ArrayList<ScheduleResult> result = new ArrayList<ScheduleResult>();
+
+    for (ObjectId id : user.getSchedules()) {
+      Schedule s = scheduleRepository.findById(id).get();
+      if (s.isCompleted()) {
+        ScheduleResult sr = new ScheduleResult();
+        sr.copy(s);
+        ArrayList<Profile> profiles = new ArrayList<Profile>();
+        for (ObjectId userId : s.getParticipants()) {
+          Profile profile = profileRepository.findById(userId).get();
+          if (profile == null)
+            return null;
+          if (profile.getUserSeq() == code) {
+            profiles.add(profile);
+            result.add(sr);
+            break;
+          }
+        }
+      }
+    }
+
+    return result;
+  }
+
+  // 타임라인 제목 필터
+  @Override
+  public List<ScheduleResult> getTimelineByTitle(int userSeq, String word) {
+    User user = userRepository.findBySeq(userSeq);
+    if (user == null)
+      return null;
+
+    ArrayList<ScheduleResult> result = new ArrayList<ScheduleResult>();
+
+    for (ObjectId id : user.getSchedules()) {
+      Schedule s = scheduleRepository.findById(id).get();
+      if (s.isCompleted() && s.getTitle() != null && s.getTitle().contains(word)) {
+        ScheduleResult sr = new ScheduleResult();
+        sr.copy(s);
+        ArrayList<Profile> profiles = new ArrayList<Profile>();
+        for (ObjectId userId : s.getParticipants()) {
+          Profile profile = profileRepository.findById(userId).get();
+          if (profile == null)
+            return null;
+          profiles.add(profile);
+        }
+        result.add(sr);
+      }
+    }
+
     return result;
   }
 
