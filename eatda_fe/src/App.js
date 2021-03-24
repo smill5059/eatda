1b10f34c846431e13afdc41d5a55ac16a92cf47b
i-cluster <jhlee9091@gmail.com>
2021-03-24 19:03:27 +0900

    Update: 프로필 UI 구조 만들기 - src/App.js : Profile 라우트 추가 - src/assets/scss/profile/scss : 프로필 scss 파일 생성 - src/index.scss : 프로필 scss 컴포넌트와 연결 - src/pages/account/profile.js : 프로필 그리드 작업 중

--- eatda_fe/src/App.js
+++ eatda_fe/src/App.js
@@ -4,6 +4,7 @@ import { Route, BrowserRouter, Switch, } from "react-router-dom";
 import Layout from './pages/layout/layout';
 import Main from './pages/main/main';
 import Login from './pages/main/login';
+import Profile from './pages/account/profile';
 // 약속 생성
 import CreateModify from './pages/meeting/createModify';
 
@@ -21,6 +22,7 @@ function App() {
             {/* <Route path="/" component={Layout} /> */}
             <Route exact path="/" component={Main} />
             <Route path="/login" component={Login} />
+            <Route path="/profile" component={Profile} />
             <Route path="/createMeeting" component={CreateModify}/>
           </Switch>
         </div>
--- eatda_fe/src/pages/account/profile.js
+++ eatda_fe/src/pages/account/profile.js
@@ -1,10 +1,25 @@
 import React from "react";
-import './App.css';
+// import './App.css';
 
 function App() {
   return (
-    <div className="App">
-
+    <div className="contentWrapper">
+      <div className="contentTitle">
+        내 정보
+      </div>
+      <div className="profileBody">
+        <div className="profileBox sizing">
+          <div className="sizing usrImg"></div>
+          <div className="sizing usrName"></div>
+          <div className="sizing usrCode"></div>
+        </div>
+        <div className="friendBox sizing">
+          <div className="sizing frdTitle"></div>
+          <div className="sizing frdAddBtn"></div>
+          <div className="sizing frdList">
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
