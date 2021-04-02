fe10f42aade63da5266901d0506cd88036fd0ee4
정수림 <smill5059@naver.com>
2021-04-02 16:08:52 +0900

    Modify: comment 등록 시 별점 추가할 수 있게 수정 - eatda_be/controller/ReviewController.java: multipartfile 오류 수정 중 - eatda_be/service/ReviewServiceImpl.java: setScore 추가

--- eatda_be/src/main/java/com/ssafy/eatda/controller/ReviewController.java
+++ eatda_be/src/main/java/com/ssafy/eatda/controller/ReviewController.java
@@ -1,16 +1,13 @@
 package com.ssafy.eatda.controller;
 
 import com.ssafy.eatda.service.ReviewService;
-import com.ssafy.eatda.vo.Comment;
 import com.ssafy.eatda.vo.Schedule;
-import com.ssafy.eatda.vo.Score;
 import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,15 +23,17 @@ public class ReviewController {
   @Autowired
   private ReviewService reviewSvc;
 
-  @PutMapping("/img/{id}")
-  public ResponseEntity<?> updateImg(@PathVariable ObjectId id,
-      @RequestParam List<MultipartFile> updatedFiles, @RequestParam List<String> deletedFiles,
-      HttpServletRequest req) {
-    Schedule result = reviewSvc.updateImg(id, updatedFiles, deletedFiles);
-    if (result != null) {
-      return new ResponseEntity<>(result, HttpStatus.OK);
-    }
-    return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+  @PutMapping("/img")
+  public ResponseEntity<?> updateImg(@RequestParam ObjectId id, @RequestParam MultipartFile updatedFiles,
+      @RequestParam List<String> deletedFiles,
+      MultipartHttpServletRequest req) {
+    System.out.println("컨트롤러 진입");
+    return new ResponseEntity<>(HttpStatus.OK);
+//    Schedule result = reviewSvc.updateImg(id, updatedFiles, deletedFiles);
+//    if (result != null) {
+//      return new ResponseEntity<>(result, HttpStatus.OK);
+//    }
+//    return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
   }
 
   @PutMapping("/comment")
