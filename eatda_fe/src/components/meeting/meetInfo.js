ceff8fc2849c83ab5770ede03696470fbfadefac
jiuen <wldms9604@naver.com>
2021-04-02 12:01:32 +0900

    Modify : 약속보기 수정  - src/pages/meeting/read.js : 약속보기 수정중  - src/components/meeting/meetInfo.js : 약속보기 전 수정중  - src/components/meeting/meetReview.js : 약속보기 후 수정중

--- eatda_fe/src/components/meeting/meetInfo.js
+++ eatda_fe/src/components/meeting/meetInfo.js
@@ -1,13 +1,15 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Button, Row, Col } from "antd";
 
-function MeetingInfo() {
+function MeetingInfo(props) {
+  const [data, setData] = useState("");
   useEffect(() => {
+    setData(props.data)
     // 지도 로딩
     const { kakao } = window;
     if (document.querySelector(".meetingReadMap") !== null) {
       new kakao.maps.Map(document.querySelector(".meetingReadMap"), {
-        center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+        center: new kakao.maps.LatLng(props.data.stores[0].storeLatitude, props.data.stores[0].storeLongitude), //지도의 중심좌표.
         level: 3, //지도의 레벨(확대, 축소 정도)
       });
     }
@@ -15,59 +17,23 @@ function MeetingInfo() {
   return (
     <div className="contentBody meetingReadContent">
       <Row justify="end" className="meetingReadTitle">
-        <p>수림이와 홍대 나들이</p>
+        <p>{data.title}</p>
       </Row>
       <Col className="meetingReadMap"></Col>
       <Col className="meetingReadStore">
-        <Row className="meetingReadStoreItem">
-          <Col span={3}></Col>
-          <Col span={21} className="meetingReadStoreName">
-            가게 이름
-          </Col>
-        </Row>
-        <Row className="meetingReadStoreItem">
-          <Col span={3}></Col>
-          <Col span={21} className="meetingReadStoreName">
-            가게 이름가게 이름가게 이름가게 이름가게 이름가게 이름가게 이름가게
-            이름가게 이름가게 이름가게 이름
-          </Col>
-        </Row>
-        <Row className="meetingReadStoreItem">
-          <Col span={3}></Col>
-          <Col span={21} className="meetingReadStoreName">
-            가게 이름
-          </Col>
-        </Row>
-        <Row className="meetingReadStoreItem">
-          <Col span={3}></Col>
-          <Col span={21} className="meetingReadStoreName">
-            가게 이름
-          </Col>
-        </Row>
+        {data.stores.map((store, i) => {
+          return (<StoreName name={store.storeName}
+            key={i} />);
+        })}
       </Col>
       <Col className="meetingReadFriend">
         <p>만나는 사람</p>
         <div className="meetingReadFriendList">
-          <Row className="meetingReadFriendItem">
-            <Col span={8} className="meetingReadFriendImage">
-              <img src="https://via.placeholder.com/30" />
-            </Col>
-            <Col span={16} className="meetingReadFriendName">
-              왕왕왕asdasdasdasdasdasdasd
-            </Col>
-          </Row>
-          <Row className="meetingReadFriendItem">
-            <Col className="meetingReadFriendImage">
-              <img src="https://via.placeholder.com/30" />
-            </Col>
-            <Col className="meetingReadFriendName">왕왕왕</Col>
-          </Row>
-          <Row className="meetingReadFriendItem">
-            <Col className="meetingReadFriendImage">
-              <img src="https://via.placeholder.com/30" />
-            </Col>
-            <Col className="meetingReadFriendName">왕왕왕</Col>
-          </Row>
+          {data.participants.map((friend, i) => {
+            return (<Friends name={friend.userName}
+              imgUrl={friend.userProfileUrl}
+              key={i} />);
+          })}
         </div>
       </Col>
       <Row className="meetingReadDone" justify="end">
@@ -75,6 +41,34 @@ function MeetingInfo() {
       </Row>
     </div>
   );
+}
+
+class StoreName extends React.Component {
+  render() {
+    return (
+      <Row className="meetingReadStoreItem">
+        <Col span={3}></Col>
+        <Col span={21} className="meetingReadStoreName">
+          {this.props.name}
+        </Col>
+      </Row>
+    );
+  }
+}
+
+class Friends extends React.Component {
+  render() {
+    return (
+      <Row className="meetingReadFriendItem">
+        <Col span={8} className="meetingReadFriendImage">
+          <img src={`${process.env.REACT_APP_API_URL}/files/${this.props.imgUrl}`} />
+        </Col>
+        <Col span={16} className="meetingReadFriendName">
+          {this.props.name}
+        </Col>
+      </Row>
+    );
+  }
 }
 
 export default MeetingInfo;
--- eatda_fe/src/pages/meeting/read.js
+++ eatda_fe/src/pages/meeting/read.js
@@ -1,11 +1,60 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Button, Dropdown, Menu, Row, Col, Image } from "antd";
 import MeetingInfo from "../../components/meeting/meetInfo";
 import MeetingReview from "../../components/meeting/meetReview"
+import { RestFilled } from "@ant-design/icons";
 
 function MeetingRead(props) {
   const { meetingId } = props.match.params;
-  console.log(meetingId);
+  const [data, setData] = useState("");
+  const [month, setMonth] = useState("");
+  const [date, setDate] = useState("");
+  const [day, setDay] = useState("");
+  const [hours, setHours] = useState("");
+  const [minutes, setMinutes] = useState("");
+
+  useEffect(() => {
+    (fetch(`${process.env.REACT_APP_API_URL}/meeting/` + meetingId, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'token': `eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJBMTAzIiwiZXhwIjoxNjE3MzQ5ODAxLCJzZXEiOjE2NjQwMzg3MTB9.MDMuYXPF16aeQnCwhiwm2n0hRr2bbNbt4H4bFNRFwYY`
+      }
+    })
+      .then(res => res.json())
+      .then(res => {
+        let date = parse(res.meetDate);
+        res.meetDate = date
+        setData(res)
+        setMonth(date.getMonth() + 1);
+        setDate(date.getDate());
+        if (date.getDay == 0) {
+          setDay("일")
+        } else if (date.getDay() == 1) {
+          setDay("월")
+        } else if (date.getDay() == 2) {
+          setDay("화")
+        } else if (date.getDay() == 3) {
+          setDay("수")
+        } else if (date.getDay() == 4) {
+          setDay("목")
+        } else if (date.getDay() == 5) {
+          setDay("금")
+        } else {
+          setDay("토")
+        }
+        setHours(date.getHours())
+        setMinutes(date.getMinutes())
+      }))
+  }, [])
+  function parse(str) {
+    var y = str.substr(0, 4);
+    var m = str.substr(5, 2);
+    var d = str.substr(8, 2);
+    var h = str.substr(11, 2);
+    var minutes = str.substr(14, 2);
+    return new Date(y, m - 1, d, h, minutes, 0);
+  }
 
   let createUrl = false;
 
@@ -26,18 +75,21 @@ function MeetingRead(props) {
   return (
     <div className="contentWrapper">
       <Row className="contentTitle">
-        <Col span={20}>3월 23일(화) 오후 5시</Col>
+        {/* <Col span={20}>3월 23일(화) 오후 5시</Col> */}
+        <Col span={20}>{month}월 {date}일 ({day}) {hours}시 {minutes}분</Col>
         <Col span={4}>
           <Dropdown overlay={menu} placement="bottomRight" trigger={["click"]}>
             <Button>...</Button>
           </Dropdown>
         </Col>
       </Row>
-      {/* 조건문으로 분할 */}
-      <MeetingInfo></MeetingInfo>
-      {/* <MeetingReview></MeetingReview> */}
 
-
+      <MeetingReview data={data}></MeetingReview>
+      {/* {data.completed ? (
+        <MeetingReview data={data}></MeetingReview>
+      ) : (
+          <MeetingInfo data={data}></MeetingInfo>
+        )} */}
     </div>
   );
 }
