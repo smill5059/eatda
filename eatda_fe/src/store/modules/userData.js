7c15d2be001b6efb1802a1a6bc530351b00078b5
i-cluster <jhlee9091@gmail.com>
2021-04-03 22:59:09 +0900

    Update : 친구삭제 데이터 바인딩, 디자인 수정

--- eatda_fe/src/store/modules/userData.js
+++ eatda_fe/src/store/modules/userData.js
@@ -1,13 +1,13 @@
 import { createAction, handleActions } from 'redux-actions'
 import profileUrl from 'assets/product/profileImg.jpg'
-import { act } from '@testing-library/react';
 
 const SET_USER = 'userData/SET_USER';
 const ADD_FRIEND = 'userData/ADD_FRIEND';
-const TEST = 'userData/TEST';
+const DELETE_FRIEND = 'userData/DELETE_FRIEND'
 
 export const setUser = createAction(SET_USER, data => ({username: data.name, usercode: data.code, friends: data.friends}));
-export const addFriend = createAction(ADD_FRIEND, data => data)
+export const addFriend = createAction(ADD_FRIEND, data => data);
+export const deleteFriend = createAction(DELETE_FRIEND, data => data);
 
 const user = {
   username: '',
@@ -16,8 +16,6 @@ const user = {
   friendList: []
 }
 
-// Object.assign(state.friendList, )
-
 const userData = handleActions(
   {
     [SET_USER]: (state, action) => ({
@@ -25,15 +23,15 @@ const userData = handleActions(
       username: action.payload.username,
       usercode: action.payload.usercode,
       friendList: action.payload.friends
-      // friendList: action.payload.friends.map(function(friend) {
-      //   let obj = {};
-      //   obj[]
-      // })
     }),
     [ADD_FRIEND]: (state, action) => ({
       ...state,
       friendList: state.friendList.concat(action.payload)
-    })
+    }),
+    [DELETE_FRIEND]: (state, action) => ({
+      ...state,
+      friendList: action.payload
+    }),
   },
   user
 )
