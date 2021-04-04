642402b9be071a64265c95b58decbda32c4471f6
jiuen <wldms9604@naver.com>
2021-04-05 01:01:14 +0900

    Modify : storeId 설정 추가  - eatda_be/vo/MaxStoreId.java : maxValue 얻기 위한 테이블 추가  - eatda_be/repository/MaxStoreIdRepository.java : maxValue 얻기 위한 repository  - eatda_be/service/MeetingServiceImpl.java : storeId 설정  - eatda_be/repository/StoreRepository.java : storeName과 storeAddress로 store 찾기

--- eatda_be/src/main/java/com/ssafy/eatda/repository/StoreRepository.java
+++ eatda_be/src/main/java/com/ssafy/eatda/repository/StoreRepository.java
@@ -1,10 +1,13 @@
 package com.ssafy.eatda.repository;
 
-import com.ssafy.eatda.vo.Store;
+import java.util.ArrayList;
 import java.util.Optional;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import com.ssafy.eatda.vo.Store;
 
 public interface StoreRepository extends MongoRepository<Store, ObjectId> {
   Optional<Store> findByStoreId(String storeId);
+
+  ArrayList<Store> findByStoreNameAndStoreAddress(String storeName, String storeAddress);
 }
---
+++ eatda_be/src/main/java/com/ssafy/eatda/vo/MaxStoreId.java
@@ -0,0 +1,20 @@
+package com.ssafy.eatda.vo;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.ssafy.eatda.util.CustomObjectIdSerializer;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MaxStoreId {
+  @JsonSerialize(using = CustomObjectIdSerializer.class)
+  @Id
+  private ObjectId id;
+  private int maxValue;
+
+}
