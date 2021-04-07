2c37f0cedf4ac7f3be6b5d8b62d51d97a71a7d96
jiuen <wldms9604@naver.com>
2021-04-07 21:17:56 +0900

    Modify : 타임라인 띄어쓰기
     - eatda_fe/components/main/timeline.js : 띄어쓰기 수정

--- eatda_fe/src/components/main/timeline.js
+++ eatda_fe/src/components/main/timeline.js
@@ -13,7 +13,7 @@ function Timeline() {
   // 검색창
   const { Search } = Input;
   const onSearch = value => (
-    console.log(value) 
+    console.log(value)
   )
 
   // 약속 정보 보기
@@ -23,45 +23,45 @@ function Timeline() {
     history.push(`/meeting/${meeting.id}`)
   }
 
-  const meetingFriends = meeting => meeting.participants.slice(0, 2).map(parti => 
+  const meetingFriends = meeting => meeting.participants.slice(0, 2).map(parti =>
     <span className="meetingFrName">
-      { parti.userName }
+      {parti.userName}
     </span>
   )
 
   // 약속 리스트 출력
   const timeline = meetings.map(meeting => {
-      let imgUrl = noImg
-      if (meeting.imgs.length > 0){
-          imgUrl = `${process.env.REACT_APP_API_URL}/files/${meeting.imgs[0]}`
-      }
+    let imgUrl = noImg
+    if (meeting.imgs.length > 0) {
+      imgUrl = `${process.env.REACT_APP_API_URL}/files/${meeting.imgs[0]}`
+    }
     return (
-    <Card.Grid key={meeting.id} onClick={() => meetingInfo(meeting)}>
-      <div className="meetingImg">
-        <Image alt="image" src={imgUrl}/>
-      </div>
-      <div className="meetingTitle">
-        { meeting.title }
-      </div>
-      <div className="meetingTime">
-        { moment(meeting.meetDate).format('YYYY - MM') }
-      </div>
-      <div className="meetingFr">
-        { meetingFriends(meeting) }
-      </div>
-      <div className="meetingLoca">
-        { meeting.stores[0].storeName }
-      </div>
-    </Card.Grid>
+      <Card.Grid key={meeting.id} onClick={() => meetingInfo(meeting)}>
+        <div className="meetingImg">
+          <Image alt="image" src={imgUrl} />
+        </div>
+        <div className="meetingTitle">
+          {meeting.title}
+        </div>
+        <div className="meetingTime">
+          {moment(meeting.meetDate).format('YYYY - MM')}
+        </div>
+        <div className="meetingFr">
+          {meetingFriends(meeting)}
+        </div>
+        <div className="meetingLoca">
+          {meeting.stores[0].storeName}
+        </div>
+      </Card.Grid>
     )
-}
+  }
   )
 
   return (
     <div className="timeline">
       <Search className="searchBar" placeholder="걔의 이름은" onSearch={onSearch} />
       <div className="meetingList">
-        { timeline }
+        {timeline}
       </div>
     </div>
   );
